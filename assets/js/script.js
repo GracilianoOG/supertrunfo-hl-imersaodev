6b455a1b726e29032d6b254534ed1331d02e8088
Gabriel <gabriel.gracilianomb@gmail.com>
2022-09-16 20:17:30 -0300

    Opção para não criar inputs do tipo radio na função geraCarta

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,24 +36,36 @@ function sorteiaCarta() {
     return listaCartas[ Math.floor(Math.random() * listaCartas.length) ];
 }
 
-function escolheCarta(carta, jogador) {
+function escolheCarta(carta, jogador, geraInputs = true) {
     const jogadorTitulo = jogador.querySelector(".carta__titulo");
     const jogadorImagem = jogador.querySelector(".carta__imagem");
     jogadorTitulo.innerHTML = carta.nome;
     jogadorImagem.src = carta.imagem;
-    geraCarta(carta, jogador);
+    geraCarta(carta, jogador, geraInputs);
 }
 
-function geraCarta(carta, jogador) {
+function geraCarta(carta, jogador, geraInputs) {
     const jogadorAtributos = jogador.querySelector(".carta__atributos");
+    jogadorAtributos.innerHTML = "";
     for(let atributo in carta.atributos) {
-        jogadorAtributos.innerHTML += 
-        `
-        <div class="atributo">
-            <input class="atributo__input" type="radio" name="atributo" id="${atributo}">
-            <label class="atributo__label" for="${atributo}">${atributo} <span class="atributo__valor">${carta.atributos[atributo]}</span></label>
-        </div>
-        `;
+        if(geraInputs) {
+            jogadorAtributos.innerHTML += 
+            `
+            <div class="atributo">
+                <input class="atributo__input" type="radio" name="atributo" id="${atributo}">
+                <label class="atributo__label" for="${atributo}">${atributo} <span class="atributo__valor">${carta.atributos[atributo]}</span></label>
+            </div>
+            `;
+        } else {
+            jogadorAtributos.innerHTML += 
+            `
+            <div class="atributo">
+                <label class="atributo__label">${atributo} <span class="atributo__valor">${carta.atributos[atributo]}</span></label>
+            </div>
+            `;
+        }
     }
-    jogadorAtributos.children[0].children[0].checked = true;
+    if(geraInputs) {
+        jogadorAtributos.children[0].children[0].checked = true;
+    }
 }
